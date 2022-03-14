30ea5751da11ce7f81393d71a9f6ee1b448845a6
Crowdbotics <team@crowdbotics.com>
2022-03-14 16:02:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "spinseed_34176",
+  url: "https://spinseed_34176.botics.co",
+  api: "https://spinseed_34176.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
